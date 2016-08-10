f8cfaed304b1ce17b12622e72d7ac669cb0cd755
shubh251996 <shubhangi.gadadare@cumminscollege.in>
2016-08-10 08:49:00 +0530

    college

--- cummins.cpp
+++ cummins.cpp
@@ -1,4 +1,5 @@
-Hiii!!!!!
+Hello!!!!!!!!!!!!!!!!!!
 Departments are in college are as follws.
 There are 5 departments in the college.
 In our college so big library is their.
+All types books are their.
